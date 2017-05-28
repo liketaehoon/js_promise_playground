550e679600874cd0324ef9cfc2e543d974bfd89c
Taehoon Koo <liketaehoon@happytechnology.kr>
2017-05-28 20:49:39 +0700

    add newphone_tut.js

---
+++ newphone_tut.js
@@ -0,0 +1,44 @@
+// following codes are show at https://scotch.io/tutorials/javascript-promises-for-dummies
+
+const isMomHappy = true;
+
+var willIGetNewPhone = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone); // fulfilled
+        } else {
+            var reason = new Error('mom is not happy');
+            reject(reason); // reject
+        }
+    }
+);
+
+var showOff = function (phone) {
+    /*    return new Promise(
+        function (resolve, reject) {
+            resolve(`Hey friend, I have a new ${phone.color} ${phone.brand} phone.`);
+        }
+    ); */
+    // shortcut for always resolve
+    //
+    return Promise.resolve(`Hey friend, I have a new ${phone.color} ${phone.brand} phone.`);
+};
+
+const askMom = () => {
+    console.log('before asking mom');
+    willIGetNewPhone
+        .then(showOff)
+        .then((fulfilled) => {
+            console.log(fulfilled)
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    console.log('after asking mom');
+};
+
+askMom();
